89ef63d2b01524eeebc969e06b84bcf9fd7ca4ac
cedric <cedricj765@gmail.com>
2023-04-07 19:47:29 -0500

    cpmleted lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+
+function superbowlWin(records){
+    let foundRecord = records.find(function(obj){
+        return obj.result === 'W'
+    }) 
+    if( foundRecord ){
+        return foundRecord.year
+    }else{
+        return undefined
+    }
+    return foundRecord
+}
+// console.log(record.find(superbowlWin))
+
+// const inventory = [
+//     { name: "apples", quantity: 2 },
+//     { name: "bananas", quantity: 0 },
+//     { name: "cherries", quantity: 5 },
+//   ];
+  
+//   function isCherries(fruit) {
+//     return fruit.name === "cherries";
+//   }
+  
+//   console.log(inventory.find(isCherries))
